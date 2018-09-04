3bb6f8710416888abf7dd99f2a74c8c2cd397a41
Andrey Savitsky <savel@gmx.com>
2018-09-04 19:00:36 +0700

    Пытаюсь сначала воспроизвести  вот этот пример https://guides.codepath.com/android/Heterogenous-Layouts-inside-RecyclerView (А потом его же и адаптировать к требованиям задания) Уже не падает причина была в том что R.id.rvContacts был в R.id.layout.activity_users

--- app/src/main/java/fb/fandroid/adv/recyclerviewapp/MainActivity.java
+++ app/src/main/java/fb/fandroid/adv/recyclerviewapp/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_users);
         // Lookup the recyclerview in activity layout
         RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
 
