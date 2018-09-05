b398297a5378b67b99b12bc318ebbf7a3f8eba97
savelgmx <savel@gmx.com>
2018-09-05 16:12:05 +0700

    Menu files and code added

--- app/src/main/java/fb/fandroid/adv/recyclerviewapp/MainActivity.java
+++ app/src/main/java/fb/fandroid/adv/recyclerviewapp/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -30,11 +33,52 @@ ViewHolder’ы должны быть легко различимы на гла�
  */
 public class MainActivity extends AppCompatActivity {
 
+    Menu menu;
+
     ArrayList<Contact> contacts;
+
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+       /*Добавляем метод, который берёт данные из ресурсов меню
+       и преобразует их в пункты меню на экране.
+       По английски "inflate" переводится как надувать,
+       т.е. по замыслу разработчиков Android, мы как бы надуваем данными объект, например, меню.
+       Но на самом деле слово "inflate" происходит от словосочетания in flat - в квартиру.
+       Существует старинная традиция запускать в квартиру первым кота,
+       который исследует все закоулки дома и заявляет о своём согласии жить в нём.
+       Так и мы запускаем данные из XML-файла в объект MenuInflater.
+        */
+        inflater.inflate(R.menu.menu_main, menu);
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // получим идентификатор выбранного пункта меню
+        int id = item.getItemId();
+
+        // Операции для выбранного пункта меню
+        switch (id) {
+            case R.id.action_add_type1_item:
+
+                //-Add Element type1  in Recycler view
+
+                //----end of launch transaction+-
+
+
+                return true;
+            case R.id.action_add_type2_item:
+             case R.id.action_exit:
+                // showMessage("Вы выбрали Выход");
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         setContentView(R.layout.activity_users);
         // Lookup the recyclerview in activity layout
         RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
